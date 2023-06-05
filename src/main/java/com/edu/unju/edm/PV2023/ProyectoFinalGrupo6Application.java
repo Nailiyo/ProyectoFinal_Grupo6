c7bf0a6354375e2ed760571bcfd445f005ebe580
Nailiyo <emiliano.aramayo14@gmail.com>
2023-06-04 23:32:14 -0300

    creando proyecto

---
+++ src/main/java/com/edu/unju/edm/PV2023/ProyectoFinalGrupo6Application.java
@@ -0,0 +1,13 @@
+package com.edu.unju.edm.PV2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalGrupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/unju/edm/PV2023/ProyectoFinalGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unju.edm.PV2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
