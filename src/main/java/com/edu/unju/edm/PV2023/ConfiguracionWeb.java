093d1069455114c517d667b599e9e6d5177290ea
BrunoLolo <brunixgroso@gmail.com>
2023-06-15 21:32:07 -0300

    ahora Cuestionario

---
+++ src/main/java/com/edu/unju/edm/PV2023/ConfiguracionWeb.java
@@ -0,0 +1,9 @@
+package com.edu.unju.edm.PV2023;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnbaleWebSecurity
+class ConfiguracionWeb {
+	
+}
---
+++ src/main/java/com/edu/unju/edm/PV2023/repository/CuestionarioRepository.java
@@ -0,0 +1,15 @@
+package com.edu.unju.edm.PV2023.repository;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edu.unju.edm.PV2023.model.Cuestionario;
+
+@Repository
+public interface CuestionarioRepository extends CrudRepository<Cuestionario, Integer>{
+
+	public ArrayList<Cuestionario> findByEstadoCuestionario (Boolean estadoCuestionario);
+	
+}
