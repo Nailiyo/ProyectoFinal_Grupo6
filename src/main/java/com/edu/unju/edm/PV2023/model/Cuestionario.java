ac22e9e35c25dd125a0f1408f947cfb64b071000
BrunoLolo <brunixgroso@gmail.com>
2023-06-16 01:07:22 -0300

    AQUI VA MODIFICA

--- src/main/java/com/edu/unju/edm/PV2023/model/Cuestionario.java
+++ src/main/java/com/edu/unju/edm/PV2023/model/Cuestionario.java
@@ -1,7 +1,5 @@
 package com.edu.unju.edm.PV2023.model;
 
-import java.util.Date;
-
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
@@ -11,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Component
 @Entity
@@ -28,63 +28,87 @@ public class Cuestionario {
     @JoinColumn(name="idAlumno")
 	Alumno alumno;
 	
-	private String nivelCuestionario;
-	private Integer puntajeCuestionario;
-	private Integer puntajeTotal;
-	private Date fechaCuestionario;
-	private Boolean estadoCuestionario;
+	@NotNull(message = "se necesita:puntajeFinal")
+	private Integer puntajeFinal;
+	@NotBlank(message= "se necesita: pregunta1")
+	private String pregunta1;
+	@NotBlank(message= "se necesita: pregunta2")
+	private String pregunta2;
+	@NotBlank(message= "se necesita: pregunta3")
+	private String pregunta3;
+	@NotBlank(message="se necesita: pregunta4")
+	private String pregunta4;
+	@NotBlank(message="se necesita: pregunta5")
+	private String pregunta5;
+	private Boolean estado;
 	
-	public Cuestionario() {
-		
-	}
-	
-	public Cuestionario(Integer idCuestionario, String nivelCuestionario, Integer puntajeCuestionario, Integer puntajeTotal, Date fechaCuestionario, Boolean estadoCuestionario) {
+	public Cuestionario(){}
+
+	public Cuestionario(Integer idCuestionario, String pregunta1,
+	String pregunta2, String pregunta3, String pregunta4, String pregunta5,
+	Docente docente, Integer puntajeFinal) {
+		super();
+		this.pregunta1 = pregunta1;
+		this.pregunta2 = pregunta2;
+		this.pregunta3 = pregunta3;
+		this.pregunta4 = pregunta4;
+		this.pregunta5 = pregunta5;
 		this.idCuestionario = idCuestionario;
-		this.nivelCuestionario = nivelCuestionario;
-		this.puntajeCuestionario = puntajeCuestionario;
-		this.puntajeTotal = puntajeTotal;
-		this.fechaCuestionario = fechaCuestionario;
-		this.estadoCuestionario = estadoCuestionario;
+		this.docente = docente;
+		this.puntajeFinal = puntajeFinal;
 	}
-	
 	public Integer getIdCuestionario() {
 		return idCuestionario;
 	}
 	public void setIdCuestionario(Integer idCuestionario) {
 		this.idCuestionario = idCuestionario;
 	}
-	public String getNivelCuestionario() {
-		return nivelCuestionario;
+	public String getPregunta1() {
+		return pregunta1;
 	}
-	public void setNivelCuestionario(String nivel) {
-		this.nivelCuestionario = nivel;
+	public void setPregunta1(String pregunta1) {
+		this.pregunta1 = pregunta1;
 	}
-	public Integer getPuntajeCuestionario() {
-		return puntajeCuestionario;
+	public String getPregunta2() {
+		return pregunta2;
 	}
-	public void setPuntajeCuestionario(Integer puntaje) {
-		this.puntajeCuestionario = puntaje;
+	public void setPregunta2(String pregunta2) {
+		this.pregunta2 = pregunta2;
 	}
-	public Integer getPuntajeTotal() {
-		return puntajeTotal;
+	public String getPregunta3() {
+		return pregunta3;
 	}
-	public void setPuntajeTotal(Integer puntajeTotal) {
-		this.puntajeTotal = puntajeTotal;
+	public void setPregunta3(String pregunta3) {
+		this.pregunta3 = pregunta3;
 	}
-	public Date getFechaCuestionario() {
-		return fechaCuestionario;
+	public String getPregunta4() {
+		return pregunta4;
 	}
-	public void setFecha(Date fechaCuestionario) {
-		this.fechaCuestionario = fechaCuestionario;
+	public void setPregunta4(String pregunta4) {
+		this.pregunta4 = pregunta4;
 	}
-
-	public Boolean getEstadoCuestionario() {
-		return estadoCuestionario;
+	public String getPregunta5() {
+		return pregunta5;
 	}
-
-	public void setEstadoCuestionario(Boolean estadoCuestionario) {
-		this.estadoCuestionario = estadoCuestionario;
+	public void setPregunta5(String pregunta5) {
+		this.pregunta5 = pregunta5;
 	}
-	
-	
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+		public Integer getPuntajeFinal() {
+		return puntajeFinal;
+	}
+	public void setPuntajeFinal(Integer puntajeFinal) {
+		this.puntajeFinal = puntajeFinal;
+	}
+	public Docente getDocente() {
+		return docente;
+	}
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}	
 }
