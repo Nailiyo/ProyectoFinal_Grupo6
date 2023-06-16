3826fc6a550e46a753b70603183103730b2b79da
Nailiyo <emiliano.aramayo14@gmail.com>
2023-06-15 23:30:26 -0300

    modelos de las nuevas tablas y arreglos del pom

--- src/main/java/com/edu/unju/edm/PV2023/model/Docente.java
+++ src/main/java/com/edu/unju/edm/PV2023/model/Docente.java
@@ -6,6 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Component
 @Entity
@@ -13,17 +17,25 @@ public class Docente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idDocente;
+    @NotNull(message="se necesita: dniDocente")
     private Integer dniDocente;
+    @NotBlank(message="se necesita: nombreDocente")
+    @Size(min=3, max = 20)
     private String nombreDocente;
+    @NotBlank(message="se necesita: apellidoDocente")
+    @Size(min=3, max = 20)
     private String apellidoDocente;
     private Boolean estadoDocente;
+    @NotBlank(message="se necesita: materiaDocente")
     private String materiaDocente;
+    @NotNull(message="se necesita: contraseñaDocente")
     private String contraseñaDocente;
-    
-    
+    @Email
+    private String emailDocente;
     public Docente(){super();}
-    public Docente(Integer idDocente, Integer dniDocente, String nombreDocente, String apellidoDocente, Boolean estadoDocente, String materiaDocente, String contraseñaDocente){
+    public Docente(Integer idDocente, Integer dniDocente, String emailDocente, String nombreDocente, String apellidoDocente, Boolean estadoDocente, String materiaDocente, String contraseñaDocente){
         super();
+        this.emailDocente = emailDocente;
         this.idDocente = idDocente;
         this.dniDocente = dniDocente;
         this.nombreDocente = nombreDocente;
@@ -31,24 +43,19 @@ public class Docente {
         this.estadoDocente = estadoDocente;
         this.materiaDocente = materiaDocente;
         this.contraseñaDocente = contraseñaDocente;
-        
     }
     public Integer getIdDocente() {
         return idDocente;
     }
-
     public void setIdDocente(Integer idDocente) {
         this.idDocente = idDocente;
     }
-
     public Integer getDniDocente() {
         return dniDocente;
     }
-
     public void setDniDocente(Integer dniDocente) {
         this.dniDocente = dniDocente;
     }
-
     public String getNombreDocente(){
         return nombreDocente;
     }
@@ -79,6 +86,10 @@ public class Docente {
 	public void setContraseñaDocente(String contraseñaDocente) {
 		this.contraseñaDocente = contraseñaDocente;
 	}
-    
-    
+    public String getemailDocente() {
+        return emailDocente;
+    }    
+    public void setemailDocente(String emailDocente) {
+        this.emailDocente = emailDocente;
+    }
 }
